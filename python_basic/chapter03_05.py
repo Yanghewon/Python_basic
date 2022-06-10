25ad60f534d0372801a48beda10bff7c28dedaa7
Yanghewon <hewon8883@naver.com>
2022-06-10 17:49:59 +0900

    test

---
+++ python_basic/chapter03_05.py
@@ -0,0 +1,135 @@
+# Chapter03-5
+# 파이썬 딕셔너리
+# 범용적으로 가장 많이 사용 (json형태)
+# 딕셔너리 자료형(순서x, 키 중복x, 수정o, 삭제o)
+
+# 복습
+"""
+[] : 리스트
+() : 튜플
+{} : 딕셔너리
+"""
+
+# 선언
+a = {'name': 'Kim', 'phone': '01033337777', 'birth': '870514'} # {'a': 'b'}, a를 '키', b를 '값'이라고 부른다
+b = {0: 'Hello Python'}
+c = {'arr': [1, 2, 3, 4]} # 리스트를 넣어도 됨
+d = {
+    'Name': 'Niceman',
+    'City': 'Seoul',
+    'Age': 33,
+    'Grade': 'A',
+    'Status': True
+}
+
+e = dict([                  # 너무 교과적
+    ('Name', 'Niceman'),
+    ('City', 'Seoul'),
+    ('Age', 33),
+    ('Grade', 'A'),
+    ('Status', True)
+])
+
+f = dict(                   # 보다 많이 사용하는 방식
+    Name = 'Niceman',
+    City = 'Seoul',
+    Age = 33,
+    Grade = 'A',
+    Status = True
+)
+
+print('a - ', type(a), a)
+print('b - ', type(b), b)
+print('c - ', type(c), c)
+print('d - ', type(d), d)
+print('e - ', type(e), e)
+print('f - ', type(f), f)
+print()
+
+# 출력
+print('a - ', a['name']) #키가 존재x ->에러발생
+print('a - ', a.get('name')) # 키가 존재x ->None처리 (키가 없어도 프로그램이 돌아감으로 많이 사용)
+print('b - ', b[0])
+print('b - ', b.get(0))
+print('f - ', f.get('City'))
+print('f - ', f.get('Age'))
+
+# 딕셔너리 추가(기존에 있는 키면 수정, 없는 키면 추가)
+a['address'] = 'seoul' # 맨 뒤에 추가
+print('a - ', a)
+# a['name'] = 'seoul' -> 같은 키를 가지고 있으면 수정함
+a['rank'] = [1,2,3]
+print('a - ', a)
+
+# 딕셔너리 길이
+print('a - ', len(a)) # 이때 'len'은 키의 개수를 셈
+print('b - ', len(b))
+print('c - ', len(c))
+print('d - ', len(d))
+print()
+
+# dict_ket, dict_valuse, dict_items : 반복문(__iter__)에서 사용 가능 
+
+print('a - ', a.keys()) # 딕셔너리의 키만 가져옴
+print('b - ', b.keys())
+print('c - ', c.keys())
+print('d - ', d.keys())
+print('a - ', list(a.keys())) # 리스트로 형변환
+print('b - ', list(b.keys()))
+
+print()
+
+print('a - ', a.values()) # 딕셔너리의 값만 가져옴
+print('b - ', b.values())
+print('c - ', c.values())
+
+print('a - ', list(a.values()))
+print('b - ', list(b.values()))
+
+print('a - ', a.items()) # 딕셔너리의 키와 값을 하나의 튜플 형태로 출력
+print('b - ', b.items())
+print('c - ', c.items())
+
+print('a - ', list(a.items()))
+print('b - ', list(b.items()))
+
+print()
+
+print('a - ', a.pop('name')) # 'name'을 꺼내옴(그 후 딕셔너리 값에서 사라짐)
+print('a - ', a)
+
+print('c - ', c.pop('arr'))
+print('c - ', c)
+
+print()
+
+print('f - ', f.popitem()) # 'popitem'은 임의로 키와 값을 하나 꺼내옴
+print('f - ', f)
+
+print()
+
+# 딕셔너리 조회
+print('a - ', 'birth' in a) # 딕셔너리에 해당하는 키 값이 있는지 알려줌
+print('d - ', 'city' in d)
+
+# 추가 & 수정
+a['test'] = 'test_dict' # 추가
+print('a - ', a)
+
+a['address'] = 'dj' # 수정
+print('a - ', a)
+
+a.update(birth='910904') # 수정(문법적으로 확실하게 설명하는 법)
+print('a - ', a)
+
+temp = {'address': 'Busan'} # 수정
+a.update(temp)
+print('a - ', a)
+
+
+
+
+
+
+
+
---
+++ python_basic/chapter03_06.py
@@ -0,0 +1,107 @@
+# Chapter03-6
+# 집합(set) 특징
+# 집합(set) 자료형(순서x, 중복x)
+
+# 선언
+a = set()
+b = set([1, 2, 3, 4])
+c = set([1, 4, 5, 6])
+d = set([1, 2, 'Pen', 'Cap', 'Plate'])
+e = {'foo', 'bar', 'baz', 'foo','quz'} # 괄호가 '딕셔너리'같아 보이지만 key가 없이 value만 있기 때문에 '집합(set)'으로 처리함
+f = {42, 'foo', (1, 2, 3), 3.141592} 
+
+print('a - ', type(a), a, 2 in a)
+print('b - ', type(b), b)
+print('c - ', type(c), c)
+print('d - ', type(d), d)
+print('e - ', type(e), e)
+print('f - ', type(f), f)
+
+# 튜플 변환(set -> tuple)
+t = tuple(b)
+print('t - ', type(t), t)
+print('t - ', t[0], t[1:3])
+print()
+
+
+#리스트 변환(set -> list)
+l = list(c)
+l2 = list(e)
+
+print('l - ', l)
+print('l2 - ', l2)
+
+# 길이
+print(len(a))
+print(len(b))
+print(len(c))
+print(len(d))
+print(len(e))
+print(len(f))
+print()
+
+# 집합 자료형 활용
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+
+print('s1 & s2 : ', s1 & s2) # s1, s2의 교집합을 출력
+print('s1 & s2 : ', s1.intersection(s2)) # intersection(교차)
+
+
+print('s1 | s2 :', s1 | s2)
+print('s1 | s2 :', s1.union(s2))
+
+print('s1 - s2 : ', s1.difference(s2)) # 차집합
+
+# 중복 원소 확인
+print('s1 & s2 : ', s1.isdisjoint(s2)) # False: 교집합o, True: 교집합x
+
+# 부분 집합 확인
+print('subset : ', s1.issubset(s2)) # s1이 s2의 부분집합인지 확인
+print('superset : ', s1.issuperset(s2)) #s2가 s1의 집합인지 확인
+
+# 추가 & 제거
+s1 = set([1, 2, 3, 4])
+s1.add(5)
+
+print('s1 - ', s1)
+
+s1.remove(2) # s1.remove(7) 원소에 없는 값을 입력하면 에러가 생긴다
+print('s1 - ', s1)
+
+s1.discard(3) # 제거 함수 / s1.discard(7) 원소에 없는 값을 입력해도 에러 발생 안 한다
+print('s1 - ', s1)
+
+s1.clear()
+print('s1 - ', s1) # 온전히 지움
+
+a = [1, 2, 3]
+a.clear()
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
